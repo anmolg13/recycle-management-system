55bdafca8f027952ab61a33f6b7dfc61058cdf7b
Anmol Goyal <goyalanmol1312@gmail.com>
2020-10-28 09:39:50 +0530

    First commit

---
+++ src/test/java/com/cognizant/RecycleManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycleManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
